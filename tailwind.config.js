f0adac1703dd0a70190421b8e130e7949be4a599
puturangga21 <puturangga21@gmail.com>
2023-02-13 22:12:47 +0800

    add hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,7 +15,9 @@ module.exports = {
             purple: "#513962",
             grey: "#5A5A5A",
          },
+         dropShadow: {
+            buttonShadow: "0px 26px 32px rgba(12, 214, 201, 0.25)",
+         },
       },
    },
-   plugins: [require("prettier-plugin-tailwindcss")],
 };
